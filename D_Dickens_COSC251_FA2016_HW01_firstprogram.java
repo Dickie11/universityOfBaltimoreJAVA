6c67992f4ff1740806cef2b211eaa2889aeac328
Dickie11 <dickens.md91@gmail.com>
2018-03-22 13:52:42 -0400

    Create D_Dickens_COSC251_FA2016_HW01_firstprogram.java

---
+++ D_Dickens_COSC251_FA2016_HW01_firstprogram.java
@@ -0,0 +1,30 @@
+
+public class D_Dickens_COSC251_FA2016_HW01_firstprogram {
+
+	public static void main(String[] args) {
+		System.out.printf("---------------------------------------------------------------------------------------------------------------------------------------------\n");
+		System.out.printf("| %-137s |\n", "                                                      DICKENS MOTORCARS");
+		System.out.printf("---------------------------------------------------------------------------------------------------------------------------------------------\n");
+		System.out.printf("| %-15s | %-36s | %-80s |\n", "product", "description", "reason for use");
+		System.out.printf("---------------------------------------------------------------------------------------------------------------------------------------------\n");
+		System.out.printf("| %-15s | %-36s | %-80s |\n", "Nissan Versa", "entry level 4 door sedan", "Great starter car, perfect for a new driver ");
+		System.out.printf("---------------------------------------------------------------------------------------------------------------------------------------------\n");
+		System.out.printf("| %-15s | %-36s | %-80s |\n", "Toyota Prius", "compact economy car", "For a buisness person that needs to drive long distance");
+		System.out.printf("---------------------------------------------------------------------------------------------------------------------------------------------\n");
+		System.out.printf("| %-15s | %-36s | %-80s |\n", "Dodge Dart", "Standard or automatic 4 door sedan", "If your new driver is looking for somthing a little more sporty but still safe");
+		System.out.printf("---------------------------------------------------------------------------------------------------------------------------------------------\n");
+		System.out.printf("| %-15s | %-36s | %-80s |\n", "Ford F150", "Mid size truck", "great work truck, lots of space and power to haul");
+		System.out.printf("---------------------------------------------------------------------------------------------------------------------------------------------\n");
+		System.out.printf("| %-15s | %-36s | %-80s |\n", "Eagle Talon", "Older car but easily upgraded", "Great car for some one looking to modify or supe up they're vehicle");
+		System.out.printf("---------------------------------------------------------------------------------------------------------------------------------------------\n");
+		System.out.printf("| %-15s | %-36s | %-80s |\n", "Jeep Wrangler", "Lifted 4x4 truck", "for the thrill seaker looking to drive in the elements");
+		System.out.printf("---------------------------------------------------------------------------------------------------------------------------------------------\n");
+		System.out.printf("| %-15s | %-36s | %-80s |\n", "Dodge Caravan", "4 door, Family Vehicle", "Van to transport up to 8 people, and has great safety ratings.");
+		System.out.printf("---------------------------------------------------------------------------------------------------------------------------------------------\n");
+		System.out.printf("| %-15s | %-36s | %-80s |\n", "Nissan GTR", "2 door extreme sports car", "extremely fast race car for the adrenaline junky");
+		System.out.printf("---------------------------------------------------------------------------------------------------------------------------------------------\n");
+		System.out.printf("| %-15s | %-36s | %-80s |\n", "Volkwagon GTI", "2 or 4 door turbo charged hatchback", "great comuter car with a lot of performance to dispel the monatony");
+		System.out.printf("---------------------------------------------------------------------------------------------------------------------------------------------\n");
+		System.out.printf("| %-15s | %-36s | %-80s |\n", "Toyota Camry", "reliable 4 door sedan", "great daily driver that gets great gas milage and will last next to forever");
+		System.out.printf("---------------------------------------------------------------------------------------------------------------------------------------------\n");
+	}}
