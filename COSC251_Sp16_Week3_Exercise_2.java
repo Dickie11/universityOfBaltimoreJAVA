cf7a8892559b616e86dc31d45b7cdeabe1e346f4
Dickie11 <dickens.md91@gmail.com>
2018-03-22 14:07:13 -0400

    Create COSC251_Sp16_Week3_Exercise_2.java

---
+++ COSC251_Sp16_Week3_Exercise_2.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class COSC251_Sp16_Week3_Exercise_2 {
+
+	public static void main(String[] args) {
+		
+		Scanner userInput = new Scanner(System.in);
+		
+		/*********
+		 *
+		 * Pick the right type of mortgage
+		 * You will write a program to pick the right type of mortgage
+		 * for a potential home buyer
+		 * 
+		 * The possible outcomes are 
+		 *   -Fixed-rate 
+		 *   -Adjustable rate 
+		 *   -Interest-only 
+		 *   -VA loan 
+		 *   -FHA loan 
+		 *   -Subprime 
+		 *   -Jumbo
+		 *   -Balloon
+		 * 
+		 * 
+		 * Subprime is for people who have credit problems and don't qualify for other loans, they
+		 * comes with overall higher rates
+		 * 
+		 * 
+		 * The following are longer term mortgage (over 10 years):
+		 * Fixed-rate,  VA loan, FHA loan
+		 *
+		 * 
+		 * 
+		 * The following are short term
+		 * Adjustable rate, Interest-only, and Balloon
+		 * 
+		 * 
+		 * 
+		 * Jumbo loans are required if the value of the mortgage is over $420,000
+		 * no matter the duration of the loan
+		 * 
+		 * A fixed rate requires that the borrower has about 10% of the value of the house to put down.
+		 * 
+		 * 
+		 * If the borrower is a vet, a VA loan is generally the best option
+		 * 
+		 * 
+		 * If the borrower is a first time buyer, doesn't have a lot to put down, and the house is in
+		 * good condition (meets FHA inspection), the FHA loan is best.
+		 *  
+		 * 
+		 * Interest only loans can be good if the borrower expects to
+		 * be earning considerable higher wages within the next 10 years
+		 * 
+		 * 
+		 * Balloon loans have better terms for up to 10 years but 
+		 * require that the loan is paid at the end of the term which
+		 * makes the borrower either pay the balance, refinance, or sell
+		 * 
+		 *
+		 *Adjustable rates change with the standard federal rate are generally preferred 
+		 *if the current interest rate is elevated 
+		 *making it likely to go down at some point.
+		 *
+		/**/
+		
+		
+	}
+
+}
