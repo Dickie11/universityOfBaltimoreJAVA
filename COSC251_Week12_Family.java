ea1f7a2e16f1d79c81d438118ae858a9d8eb06d4
Dickie11 <dickens.md91@gmail.com>
2018-03-22 14:04:11 -0400

    Create COSC251_Week12_Family.java

---
+++ COSC251_Week12_Family.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class COSC251_Week12_Family {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		FamilyMember fMember1 = new FamilyMember();
+		
+		/*
+		fMember1.firstName = "Bart";
+		fMember1.lastName = "Simpson";
+		fMember1.age = 10;
+		
+		fMember2.firstName = "Lisa";
+		fMember2.lastName = "Simpson";
+		fMember2.age = 8;
+		*/
+		/* *
+		 * Week 12 - classes and objects
+		 * 
+		 * Exercise 1
+		 *
+		 *The class FamilyMember contains data for names and ages
+		 *use encapsulation and create the SetName and SetAge methods
+		 *allowing the following code to run and preventing the code from lines 11-17 to run.
+		 */
+		/*fMember1.SetName("Bart", "Simpson");
+		fMember2.SetName("Lisa", "Simpson");
+			
+		fMember1.SetAge(10);
+		fMember2.SetAge(8);
+		*/
+		fMember1.AboutMe();	
+	
+		
+		
+		
+	}
+	
